04994a6387e98c9bbc212a4a0044293c6ca1f517
tienhoai68 <nguyentien1668@gmail.com>
2023-10-10 17:58:22 +0700

    upload taiwindcss

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import logo from "../assets/img/Airbnb_Logo.png"
 
 export default function Header() {
   return (
-    <h1 class="text-3xl font-bold underline w-2.5">
+    <h1 className="text-3xl font-bold underline w-2.5">
       Hello world!
     </h1>
   )
